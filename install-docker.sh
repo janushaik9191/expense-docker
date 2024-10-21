c701d150e94715318d88e7943adec87fe2f556de
meerjan <janushaik9191@gmail.com>
2024-10-21 22:29:44 +0530

    docker installation

---
+++ install-docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+usermod -aG docker ec2-user
